b28e3b745e1f23d0f0b6a823423cdbbc71b6e36d
Wiggin <linnhtetaung266742@gmail.com>
2024-01-09 14:53:56 +0630

    learning page

---
+++ src/main/java/com/example/bookshop/controller/BookController.java
@@ -0,0 +1,19 @@
+package com.example.bookshop.controller;
+
+import com.example.bookshop.dao.BookDao;
+import com.example.bookshop.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+@Controller
+@RequiredArgsConstructor
+public class BookController {
+
+
+
+//    public String listBooks(Model model){
+//        model.addAttribute("books",service.bookList());
+//        return null;
+//    }
+}
--- src/main/java/com/example/bookshop/controller/HomeController.java
+++ src/main/java/com/example/bookshop/controller/HomeController.java
@@ -1,13 +1,20 @@
 package com.example.bookshop.controller;
 
+import com.example.bookshop.service.BookService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
+    private final BookService service;
+
     @GetMapping({"/","home"})
-    public String home(){
+    public String home(Model model){
+        model.addAttribute("books",service.bookList());
         return "index";
     }
 }
