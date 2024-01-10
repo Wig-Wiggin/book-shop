0202463d1498ca0aff3be1c7661f66647e273b55
Wiggin <linnhtetaung266742@gmail.com>
2024-01-10 10:30:28 +0630

    book-details

--- src/main/java/com/example/bookshop/controller/BookController.java
+++ src/main/java/com/example/bookshop/controller/BookController.java
@@ -1,19 +1,48 @@
 package com.example.bookshop.controller;
 
 import com.example.bookshop.dao.BookDao;
+import com.example.bookshop.entity.BookId;
 import com.example.bookshop.service.BookService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Objects;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/book")
 public class BookController {
 
+    public  final BookService service;
 
 
-//    public String listBooks(Model model){
-//        model.addAttribute("books",service.bookList());
-//        return null;
-//    }
+    @GetMapping("/list-books")
+    public String listBooks(Model model){
+        model.addAttribute("books",service.bookList());
+        return "listbooks";
+    }
+
+    @GetMapping("/book-details")
+    public String bookDetails(@RequestParam("id")Integer id, @RequestParam("isbn")String isbn,Model model){
+        BookId bookId = null;
+        if ((Objects.nonNull(id)) && (Objects.nonNull(isbn))){
+            bookId = new BookId();
+            bookId.setId(id);
+            bookId.setIsbn(isbn);
+        }else {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        model.addAttribute("book",service.findById(bookId));
+
+        return "bookdetails";
+
+    }
 }
--- src/main/java/com/example/bookshop/dao/BookDao.java
+++ src/main/java/com/example/bookshop/dao/BookDao.java
@@ -5,4 +5,7 @@ import com.example.bookshop.entity.BookId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookDao extends JpaRepository<Book, BookId> {
+
+
+
 }
